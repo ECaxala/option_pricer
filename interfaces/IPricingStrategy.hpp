8f89e54051b5691bf8eb8b44b8ed958a66147664
Evandro Caxala <caxala.evandro@gmail.com>
2025-08-21 14:38:28 +0200

    Add strategy inteface class

---
+++ interfaces/IPricingStrategy.hpp
@@ -0,0 +1,24 @@
+#ifndef IPRICINGSTRATEGY_HPP
+#define IPRICINGSTRATEGY_HPP
+
+#include <string>
+#include "Option.hpp"
+
+/**
+ * @brief Common interface for option pricing strategies:
+ * - Black-Scholes (exact solution)
+ * - ...tbd
+ */
+class IPricingStrategy
+{
+public: 
+    virtual ~IPricingStrategy() = default;
+
+    virtual double calculateCallPrice(const Option& option) const = 0;
+    virtual double calculatePutPrice(const Option& option) const = 0;
+
+    // Utility function
+    virtual std::string getName() const = 0;
+};
+
+#endif // IPRICINGSTRATEGY_HPP
