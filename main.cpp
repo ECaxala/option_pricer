c0ca1da7d1ac1be6c52b3b7ca43bd689c2fb467c
Evandro Caxala <caxala.evandro@gmail.com>
2025-08-22 16:04:23 +0200

    Add boilerplate code in order to test application

--- main.cpp
+++ main.cpp
@@ -1,18 +1,58 @@
 // STL
 #include <iostream>
 #include <vector>
+#include <cassert>
 // Boost
 #include <boost/random.hpp>
+#include <boost/math/distributions/normal.hpp>
 
-#include "plain_option/EuropeanOption.hpp"
+#include "OptionContext.hpp"
+#include "BlackScholesPricer.hpp"
+#include "Option.hpp"
+
+struct TestBatch
+{
+    std::string name;
+    Option option;
+    double expectedCallPrice;
+    double expectedPutPrice;
+};
+    
 
 int main(void)
-{
-    // Create a EuropeanOption instance
-    EuropeanOption option(1.0, 100.0, 0.2, 0.05, 100.0);
+{    
+    // Create test batches
+    TestBatch Batch_1 = {"BATCH 1", Option(0.25, 65.0, 0.30, 0.08, 60.0), 2.13337, 5.84628};
+    TestBatch Batch_2 = {"BATCH 2", Option(1.0, 100.0, 0.2, 0.0, 100.0), 7.96557, 7.96557};
+    TestBatch Batch_3 = {"BATCH 3", Option(1.0, 10.0, 0.50, 0.12, 5.0), 0.204058, 4.07326};
+    TestBatch Batch_4 = {"BATCH 4", Option(30.0, 100.0, 0.30, 0.08, 100.0), 92.17570, 1.24750};
 
-    // Print the option details
-    std::cout << option.ToString() << std::endl;
+    // Create a vector of test batches
+    std::vector<TestBatch> batches = {Batch_1, Batch_2, Batch_3, Batch_4};
 
-    return 0;
+    // Setup black scholes strategy
+    auto blackScholesStrategy = std::make_unique<BlackScholesPricer>();
+
+    // Init context with strategy
+    OptionContext context(std::move(blackScholesStrategy));
+
+    // Test Section A, a): Calculate exact plain option price 
+    for (const auto& batch : batches)
+    {
+        std::cout << "=== " << batch.name << " ===" << std::endl;
+        std::cout << "Parameters: " << batch.option.toString() << std::endl;
+
+        // Calculate prices
+        double callPrice = context.calculateCallPrice(batch.option);
+        double putPrice = context.calculatePutPrice(batch.option);
+
+        std::cout << "Call Price: " << callPrice << " (Expected: " << batch.expectedCallPrice << ")" << std::endl;
+        std::cout << "Put Price: " << putPrice << " (Expected: " << batch.expectedPutPrice << ")" << std::endl;
+        std::cout << std::endl;
+
+        // Assert using precision 1e-5 since only five decimal places are provided
+        assert(std::abs(callPrice - batch.expectedCallPrice) < 1e-5);
+        assert(std::abs(putPrice - batch.expectedPutPrice) < 1e-5);
+    }
+
 }
